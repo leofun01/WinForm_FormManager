55d68b249a67c46cda542784476a7828db0b0485
LeoFun01 <leofun01@users.noreply.github.com>
2018-05-23 13:54:07 +0300

    Files upload.
    Destructor & Dispose removed.

--- WinForm_FormManager/BaseForm.cs
+++ WinForm_FormManager/BaseForm.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Windows.Forms;
 
-namespace WinForm_FormManager {
+namespace OptKurs_v1_WinForm {
 	public class BaseForm : Form {
 		public BaseForm() {
 			FormManager.Instance.Add(this);
 		}
+		/*//
 		~BaseForm() {
 			FormManager.Instance.Remove(this);
 		}
-
 		protected override void Dispose(bool disposing) {
 			FormManager.Instance.Remove(this);
 			base.Dispose(disposing);
 		}
+		//*/
 		protected override void OnClosed(EventArgs e) {
 			FormManager.Instance.Remove(this);
 			base.OnClosed(e);
